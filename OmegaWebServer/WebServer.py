66212a1baa5037ccb5f930b3498ff71f8911584c
unknown <alexy.torresa@gmail.com>
2019-09-17 18:07:50 -0400

    Migrated the Omega web server

---
+++ OmegaWebServer/WebServer.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+
+####################################
+# IMPORTS
+####################################
+
+from flask     import Flask, request, render_template
+from flask_api import status
+
+####################################
+# SERVER STATE VARIALBES
+####################################
+
+# We have to keep the Flask servAPP as global var
+servApp = Flask(__name__)
+
+####################################
+# ROUTES
+####################################
+
+@servApp.route('/')
+def index():
+    return render_template("index.html", objId=3)
+
+@servApp.route('/<int:objid>')
+def indexId(objid):
+    return render_template("index.html", objId=objid)
+
+
+####################################
+# MAIN
+####################################
+if __name__ == '__main__': 
+    print("#============================#")
+    print("|    Starting the server     |")
+    print("#============================#")
+    servApp.run(debug=True, host='0.0.0.0')
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DomoHome
-Domotic web based application. 
+Domotic web based application. This first version allows to:
+* Switch ON/OFF objects
+* Apply PWM to objects
+* Get room temperature
+* Manage RGB LED strips
+
+## Hardware
+* Onion Omega 2+
+* NodeMCU V3 ESP8266 
+
+## Languages used
+* Web server and internals: Python, C++
+* Web clients: HTML/CSS/JS
