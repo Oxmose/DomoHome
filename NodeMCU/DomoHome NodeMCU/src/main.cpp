3d787ba41c8dd79a8c1c9b5dec39e13694018702
Oxmose <alexy.torresa@gmail.com>
2021-01-05 13:10:38 -0500

    Added luminosity sensor management

--- NodeMCU/DomoHome NodeMCU/src/main.cpp
+++ NodeMCU/DomoHome NodeMCU/src/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * NodeMCU client code.
+ * 
+ * Developped for the DomoHome projet.
+ * 
+ * Alexy Torres Aurora Dugo
+ */
+
 #include <Arduino.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -7,6 +15,38 @@
 #define NET_TIMEOUT 1000
  
 ESP8266WebServer server(80);
+
+/******************************************************************************
+ * UTILS
+ *****************************************************************************/
+void getToken(uint8_t* tokens, uint8_t count, String str, char sep)
+{
+  uint8_t  i;
+  uint32_t lastPos;
+  uint8_t  found;
+
+  lastPos = 0;
+  found   = 0;
+  for(i = 0; i < str.length(); ++i)
+  {
+    if(str[i] == sep)
+    {
+      tokens[found] = str.substring(lastPos, i).toInt();
+      lastPos = i + 1;
+      ++found;
+    }
+    else if(i == str.length() - 1)
+    {
+      tokens[found] = str.substring(lastPos, i + 1).toInt();
+      ++found;
+    }
+
+    if(found == count)
+    {
+      break;
+    }
+  }
+}
 
 bool isGPIOValid(uint8_t gpio)
 {
@@ -45,24 +85,35 @@ void setToggle(uint8_t gpio, bool value)
   digitalWrite(gpio, value ? HIGH : LOW);
 }
  
+/******************************************************************************
+ * REST API ROUTES
+ *****************************************************************************/
 void unknownRoute(void) 
 {
   uint8_t i;
+  uint8_t argsCount;
   String  message;
   
-  message = "Unknown route\n";
-  message += "URI: ";
+  message = "{error: 404, msg: \"Unknown route\", ";
+  message += "uri: ";
   message += server.uri();
-  message += "\nMethod: ";
+  message += ", method: \"";
   message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
+  message += "\", arguments: ";
   message += server.args();
-  message += "\n";
-  for (i = 0; i < server.args(); ++i) 
+  message += ", args: \"";
+
+  argsCount = server.args();
+  for (i = 0; i < argsCount; ++i) 
   {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    message += server.argName(i) + ": " + server.arg(i);
+    if(i != argsCount - 1)
+    {
+      message += ", ";
+    }
   }
-  server.send(404, "text/plain", message);
+  message += "\"}";
+  server.send(404, "text/json", message);
 }
 
 void defaultRoute(void) 
@@ -73,6 +124,13 @@ void defaultRoute(void)
   message += "<h2>IP: "; 
   message += WiFi.localIP().toString();
   message += "</h2>";
+  message += "<h3>Available routes: </h3>";
+  message += "<ul>";
+  message += "<li>setToggle -> gpio, value</li>";
+  message += "<li>setPWM -> gpio, value</li>";
+  message += "<li>setRGB -> gpioList, valueList</li>";
+  message += "<li>getLum</li>";
+  message += "</ul>";
   
   server.send(200, "text/html", message);
 }
@@ -122,35 +180,6 @@ void setPWMRoute(void)
   }
 
   server.send(200, "text/json", message);
-}
-
-void getToken(uint8_t* tokens, uint8_t count, String str, char sep)
-{
-  uint8_t  i;
-  uint32_t lastPos;
-  uint8_t  found;
-
-  lastPos = 0;
-  found   = 0;
-  for(i = 0; i < str.length(); ++i)
-  {
-    if(str[i] == sep)
-    {
-      tokens[found] = str.substring(lastPos, i).toInt();
-      lastPos = i + 1;
-      ++found;
-    }
-    else if(i == str.length() - 1)
-    {
-      tokens[found] = str.substring(lastPos, i + 1).toInt();
-      ++found;
-    }
-
-    if(found == count)
-    {
-      break;
-    }
-  }
 }
 
 void setRGBRoute(void)
@@ -262,6 +291,21 @@ void setToggleRoute(void)
   server.send(200, "text/json", message);
 }
 
+void getLumRoute(void)
+{
+  String message;
+  uint32 lum;
+
+  lum = analogRead(A0);
+
+  message =  "{ error: 0, value: " + String(lum) + "}";
+  
+  server.send(200, "text/json", message);
+}
+
+/******************************************************************************
+ * MAIN
+ *****************************************************************************/
 void initConnection(void)
 {
   /* Set mode and network data */
@@ -298,6 +342,9 @@ void initServerRouting(void)
 
   /* Set RGB value */
   server.on("/setRGB", HTTP_GET, setRGBRoute);
+
+  /* Read luminosity */
+  server.on("/getLum", HTTP_GET, getLumRoute);
 }
 
 void setup(void) 
@@ -321,5 +368,6 @@ void setup(void)
  
 void loop(void) 
 {
+  /* Wait for request */
   server.handleClient();
 }
