34994afb9487ee64d76ce63d01d0ba36e2c8b7ea
Oxmose <alexy.torresa@gmail.com>
2021-01-05 14:08:19 -0500

    Cleaned MCU code and removed old proejct

--- Old/NodeMCUClient/src/Logger.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOGGER_H_
-#define __LOGGER_H_
-
-void logInit(void);
-void logInfo(const char* msg);
-void logWarning(const char* msg);
-void logError(const char* msg);
-
-#endif /* #ifndef __LOGGER_H_ */
